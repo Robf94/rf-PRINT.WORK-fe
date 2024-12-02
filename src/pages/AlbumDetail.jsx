5c4b6a66346c47314a184ae69c8bb2e4347fbb0a
Rob Friskney <rfriskney94@gmail.com>
2024-12-02 10:50:54 +0000

    fetching and rendering album data

---
+++ src/pages/AlbumDetail.jsx
@@ -0,0 +1,5 @@
+function AlbumDetail() {
+  return <h1>This is the album detail page</h1>
+}
+
+export default AlbumDetail
---
+++ src/pages/Top100Albums.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { fetchTop100Albums } from "../../utils/api";
+import AlbumCard from "../components/AlbumCard";
+
+function AlbumsPage() {
+  const [albums, setAlbums] = useState([]);
+  // const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    fetchTop100Albums()
+      .then(({ data }) => {
+        const albums = data.albums;
+        // console.log(albums, "<<< albumsData");
+        setAlbums(albums);
+      })
+      .catch((err) => {
+        console.log(err, "<<< error");
+        // setIsError(true)
+      });
+  }, []);
+
+  // if (isError) {
+  //   return <ErrorPage />
+  // }
+
+  // if (isLoading) {
+  //   return <Loader />
+  // }
+
+  return (
+    <>
+      <h1>Top 100 Albums</h1>
+      {albums.map((album) => (
+        <AlbumCard
+          key={album.id}
+          albumId={album.id}
+          album={album.name}
+          artist={album.artistName}
+        />
+      ))}
+    </>
+  );
+}
+
+export default AlbumsPage;
