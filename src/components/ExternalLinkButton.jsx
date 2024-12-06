793591740a1cfea68561ee16af3ea3a1431b4c6d
Rob Friskney <rfriskney94@gmail.com>
2024-12-06 16:35:17 +0000

    Fixed external link buttons not being easily tappable

--- src/components/ExternalLinkButton.jsx
+++ src/components/ExternalLinkButton.jsx
@@ -1,15 +1,13 @@
 function ExternalButton(props) {
   const { url, btnText } = props;
   return (
-    <button className="btn rounded bg-secondary flex-1 border-none">
-      <a
-        href={url}
-        target="blank"
-        rel="noopener noreferrer"
-      >
-        {btnText}
-      </a>
-    </button>
+    <a
+      href={url}
+      target="blank"
+      rel="noopener noreferrer"
+    >
+      <button className="btn rounded bg-secondary flex-1 border-none">{btnText}</button>
+    </a>
   );
 }
 
