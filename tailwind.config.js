4bc00d84c63f68e1da3c77cf9439cd0d69306b9a
Rob Friskney <rfriskney94@gmail.com>
2024-12-07 22:02:14 +0000

    fixed module import error in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -33,5 +35,5 @@ export default {
       },
     ],
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [typography, daisyui],
 };
